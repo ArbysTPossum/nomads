f174ff82a690206287b2324b9564fede68b6f6f7
ArbysTPossum <ArbysTPossum@Gmail.com>
2025-07-17 18:57:27 -0500

    Update XNL0209_script.lua
    Change TMD to UEF style.

--- units/XNL0209/XNL0209_script.lua
+++ units/XNL0209/XNL0209_script.lua
@@ -2,12 +2,11 @@
 
 local NomadsEffectTemplate = import('/lua/nomadseffecttemplate.lua')
 local NConstructionUnit = import('/lua/nomadsunits.lua').NConstructionUnit
-local NAMFlakWeapon = import('/lua/nomadsweapons.lua').NAMFlakWeapon
+local NAMFlakWeapon = import("/lua/terranweapons.lua").TAMPhalanxWeapon
 
 XNL0209 = Class(NConstructionUnit) {
     Weapons = {
         MainGun = Class(NAMFlakWeapon) {
-            SalvoReloadTime = 1.4, --Change this to the correct amount for the weapon.
             TMDEffectBones = { 'TMD_Fx1', 'TMD_Fx2', },
         },
     },
