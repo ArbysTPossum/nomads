0352835500c5ac1f4509dd14acc7337699caf300
ArbysTPossum <ArbysTPossum@Gmail.com>
2025-07-17 18:40:34 -0500

    Update XNB4204_script.lua
    Changing Tac defense to UEF-style because Nomads is buggy.

--- units/XNB4204/XNB4204_script.lua
+++ units/XNB4204/XNB4204_script.lua
@@ -1,6 +1,6 @@
 local AddLights = import('/lua/nomadsutils.lua').AddLights
 local NStructureUnit = import('/lua/nomadsunits.lua').NStructureUnit
-local NAMFlakWeapon = import('/lua/nomadsweapons.lua').NAMFlakWeapon
+local NAMFlakWeapon = import("/lua/terranweapons.lua").TAMPhalanxWeapon
 
 NStructureUnit = AddLights(NStructureUnit)
 
@@ -10,7 +10,6 @@ XNB4204 = Class(NStructureUnit) {
     Weapons = {
         Turret01 = Class(NAMFlakWeapon) {
             TMDEffectBones = {'RadarDish',},
-            SalvoReloadTime = 1.4, --Change this to the correct amount for the weapon.
         },
     },
 
